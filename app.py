5072297394a6c9819e5a63e1b16d04f9a6cb55da
Paul Ruiz <ruizp3183@gmail.com>
2024-07-08 17:28:29 -0500

    entrenando al modelo con respuestas específicas, quien es el y quien es su entrenador

--- app.py
+++ app.py
@@ -19,15 +19,30 @@ def index():
 
 @app.route('/ask', methods=['POST'])
 def ask():
-    user_message = request.json['message']
+    user_message = request.json['message'].lower()
+    
+    personal_questions = ["¿quién eres?", "¿qué eres?", "¿qué haces?", "¿a qué te dedicas?", "¿qué resuelves?", "quien eres?", "quien eres", "que eres?", "que eres"]
+    personal_response = "Hola, soy un modelo de IA entrenado por Paul Martin Ruiz Guardia para asistir en la resolución de dudas y proporcionar información útil."
+    
+# Respuestas personalizadas para preguntas sobre Paul Martin Ruiz Guardia
+    paul_questions = ["quien es paul martin ruiz guardia", "quien es paul ruiz", "quien es paul martin ruiz", "quien es ruiz guardia"]
+    paul_response = "Paul Martin Ruiz Guardia es un Data Engineer con amplia formación en desarrollo web y manejo de APIs. Él me entrenó para ser su asistente personal, demostrando su dominio en el manejo de la inteligencia artificial."
+
+    if any(question in user_message for question in personal_questions):
+        return jsonify({'response': personal_response})
+    elif any(question in user_message for question in paul_questions):
+        return jsonify({'response': paul_response})
+    
     headers = {
         'Content-Type': 'application/json', 
         'Authorization': f'Bearer {OPENAI_API_KEY}'
     }
+
     data = {
         'model': 'gpt-3.5-turbo', 
         'messages': [{'role': 'user', 'content': user_message}]
     }
+
     try:
         response = requests.post(OPENAI_API_URL, headers=headers, json=data)
         if response.status_code == 200:
